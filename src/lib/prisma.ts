a579c8185ec143d0d4b589c2ef9bc36495d6e6bf
Jhon Pihawian <fitra13@Jhons-MacBook-Pro.local>
2026-08-26 11:45:16 +0800

    fix: reset prisma global instance for next.js hmr

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,11 @@
 import { PrismaClient } from "@prisma/client"
 
-const globalForPrisma = global as unknown as { prisma_v2: PrismaClient };
+const globalForPrisma = global as unknown as { prisma_v3: PrismaClient };
 
 export const prisma =
-  globalForPrisma.prisma_v2 ||
+  globalForPrisma.prisma_v3 ||
   new PrismaClient({
     log: ["query"],
   });
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma_v2 = prisma;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma_v3 = prisma;
